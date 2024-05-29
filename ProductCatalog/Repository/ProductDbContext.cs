66b7f0a3d77bc67c1c1e1c7397187b2f2c13f7dc
srdjan.milovanovic <srdjan.milovanovic@wm.rs>
2024-05-29 23:18:29 +0200

    Add AutoFac package, Add EntityFramework package. Add ProductDbContext

---
+++ ProductCatalog/Repository/ProductDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ProductCatalog.Models;
+using System.Data.Entity;
+
+namespace ProductCatalog.Repository
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext() : base("Server=PC300;Database=ProductCatalog;Trusted_Connection=True;")
+        {
+            Database.SetInitializer<ProductDbContext>(null);
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+    }
+}
