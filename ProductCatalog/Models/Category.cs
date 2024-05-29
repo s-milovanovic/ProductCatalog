c64efd0c52a551ed906537e3d79bf8fbb8782072
srdjan.milovanovic <srdjan.milovanovic@wm.rs>
2024-05-29 22:57:04 +0200

    Add Props to Category, Manufacturer and Supplier, Add Data Annotations

--- ProductCatalog/Models/Category.cs
+++ ProductCatalog/Models/Category.cs
@@ -1,6 +1,11 @@
-﻿namespace ProductCatalog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductCatalog.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- ProductCatalog/Models/Product.cs
+++ ProductCatalog/Models/Product.cs
@@ -1,7 +1,10 @@
-﻿namespace ProductCatalog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductCatalog.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
