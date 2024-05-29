8d9f8f9cae66ff0352dda8dbe8e3bd6d5e062c89
srdjan.milovanovic <srdjan.milovanovic@wm.rs>
2024-05-29 23:02:23 +0200

    Add ProductRepository Interface

---
+++ ProductCatalog/Service/IProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Service
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync(CancellationToken cancellationToken);
+
+        Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken);
+
+        Task<int> InsertProductAsync(Product product, CancellationToken cancellationToken);
+
+        Task<bool> UpdateProductAsync(Product product, CancellationToken cancellationToken);
+
+        Task<IEnumerable<Category>> GetAllProductCategoriesAsync(CancellationToken cancellationToken);
+
+        Task<IEnumerable<Supplier>> GetAllSuppliersAsync(CancellationToken cancellationToken);
+
+        Task<IEnumerable<Manufacturer>> GetAllManufacturersAsync(CancellationToken cancellationToken);
+    }
+}
