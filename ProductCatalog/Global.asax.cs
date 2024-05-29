56a6998486608de9942024571ea158623e0a7808
srdjan.milovanovic <srdjan.milovanovic@wm.rs>
2024-05-29 23:24:55 +0200

    Add ProductDbRepository, Register ProductDbContext, Register ProductDbRepository dependency

--- ProductCatalog/Global.asax.cs
+++ ProductCatalog/Global.asax.cs
@@ -2,7 +2,12 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using ProductCatalog.Repository;
 using ProductCatalog.Service;
+using System.Web.Http;
 
 namespace ProductCatalog
 {
@@ -10,6 +15,31 @@ namespace ProductCatalog
     {
         protected void Application_Start()
         {
+            // Register dependencies
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Register API controllers using assembly scanning.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Register ProductDbContext
+            builder.RegisterType<ProductDbContext>().AsSelf().InstancePerRequest();
+
+            // Register your service dependencies.
+            builder.RegisterType<ProductDbRepository>().As<IProductRepository>().InstancePerRequest();
+
+            // Build the container.
+            var container = builder.Build();
+
+            // Set the dependency resolver for MVC.
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            // Set the dependency resolver for Web API.
+            var configuration = GlobalConfiguration.Configuration;
+            configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ ProductCatalog/Repository/ProductDbRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ProductCatalog.Models;
+using ProductCatalog.Service;
+
+namespace ProductCatalog.Repository
+{
+    public class ProductDbRepository : IProductRepository
+    {
+        public Task<IEnumerable<Product>> GetAllProductsAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> InsertProductAsync(Product product, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UpdateProductAsync(Product product, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllProductCategoriesAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Supplier>> GetAllSuppliersAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Manufacturer>> GetAllManufacturersAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
