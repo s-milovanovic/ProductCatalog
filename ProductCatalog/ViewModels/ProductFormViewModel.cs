5322447ff889d67cf98cf28db9843e9c0e910558
srdjan.milovanovic <srdjan.milovanovic@wm.rs>
2024-05-29 23:53:42 +0200

    Add ProductFormViewModel

---
+++ ProductCatalog/ViewModels/ProductFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ProductCatalog.Models;
+using System.Collections.Generic;
+
+namespace ProductCatalog.ViewModels
+{
+    public class ProductFormViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Manufacturer> Manufacturers { get; set; }
+        public IEnumerable<Supplier> Suppliers { get; set; }
+        public Product Product { get; set; }
+    }
+}
