7aec15e557161476571941f960a89d832a847b39
srdjan.milovanovic <srdjan.milovanovic@wm.rs>
2024-05-30 21:44:31 +0200

    Create API ProductsController

---
+++ ProductCatalog/Controllers/Api/ProductsControllerApi.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using ProductCatalog.Service;
+
+namespace ProductCatalog.Controllers.Api
+{
+    public class ProductsControllerApi : ApiController
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsControllerApi(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    }
+}
