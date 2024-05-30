abdb33bf27641dd2b75cf311bc593287a7a6fe41
srdjan.milovanovic <srdjan.milovanovic@wm.rs>
2024-05-30 23:15:20 +0200

    Add IProductRepository interface to ProductFileRepository, Implement members

--- ProductCatalog/Repository/ProductFileRepository.cs
+++ ProductCatalog/Repository/ProductFileRepository.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using ProductCatalog.Models;
+using ProductCatalog.Service;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ProductCatalog.Repository
 {
-    public class ProductFileRepository
+    public class ProductFileRepository : IProductRepository
     {
+        public Task<IEnumerable<Product>> GetAllProductsAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertProductAsync(Product product, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProductAsync(Product product, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllProductCategoriesAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Supplier>> GetAllSuppliersAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Manufacturer>> GetAllManufacturersAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
