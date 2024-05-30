29d6affd4c69455d6c8c400daf51da684adb2b13
srdjan.milovanovic <srdjan.milovanovic@wm.rs>
2024-05-30 23:13:31 +0200

    Add ProductFileRepository

---
+++ ProductCatalog/Repository/ProductFileRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductCatalog.Repository
+{
+    public class ProductFileRepository
+    {
+    }
+}
