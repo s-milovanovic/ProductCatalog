7876bdd5906a05241e5be06d4a344f7757f8a6ca
srdjan.milovanovic <srdjan.milovanovic@wm.rs>
2024-05-30 21:29:15 +0200

    Add Product validations on model level

--- ProductCatalog/Models/Product.cs
+++ ProductCatalog/Models/Product.cs
@@ -4,16 +4,26 @@ namespace ProductCatalog.Models
 {
     public class Product
     {
-        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [Required]
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+
+        [Required]
         public int ManufacturerId { get; set; }
         public Manufacturer Manufacturer { get; set; }
+
+        [Required]
         public int SupplierId { get; set; }
         public Supplier Supplier { get; set; }
+
+        [Range(1, 999999999, ErrorMessage = "Price must be between 1 and 999999999.")]
         public decimal? Price { get; set; }
     }
 }
